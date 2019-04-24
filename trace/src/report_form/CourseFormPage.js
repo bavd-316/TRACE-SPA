480992d0cd4b1b2bc106604cb4b6c19baede3e1e
Daniel Guddemi <danguddemi@gmail.com>
2019-04-24 08:16:33 -0400

    Adding CourseFormPage

--- trace/src/report_form/CourseFormPage.js
+++ trace/src/report_form/CourseFormPage.js
@@ -2,54 +2,82 @@ import React, {Component} from 'react';
 import styles from "./CourseFormPage.css";
 import CourseForm from "./CourseForm";
 
-
 class CourseFormPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
             index: 0,
-            questions: "Course Related Questions"
+            questions: {
+                "Course Related Questions": [
+                    'Question1', 'Question2', 'Question3', 'Question4', 'Question5'],
+                "Instructor Related Questions": [
+                    "Question6", "Question7", "Question8"
+                ],
+                "Stupid Questions": [
+                    "Question9", "Question10", "Question11", "Question12",
+                    "Question13", "Question14", "Question15", "Question16"
+                ]
+            }
         }
-    }
+    };
 
-    updateQuestions = (iterate) => {
-        const sections = [
-            "Course Related Questions",
-            "Instructor Related Questions",
-            "Stupid Questions"];
-        let page = this.state.index;
-        page += iterate;
-        this.setState({index: page})
-        this.setState({questions: sections[page]})
-    }
+    getQuestionKey = () => {
+        let keys = Object.keys(this.state.questions);
+        return keys[this.state.index]
+    };
 
-    getQuestions = () => {
-        const questions = {
-            "Course Related Questions": [
-                'Question1', 'Question2', 'Question3', 'Question4', 'Question5'],
-            "Instructor Related Questions": [
-                "Question6", "Question7", "Question8"
-            ],
-            "Stupid Questions": [
-                "Question9", "Question10", "Question11", "Question12",
-                "Question13", "Question14", "Question15", "Question16"
-            ]
-        };
-        console.log(this.state);
-        return questions[this.state.questions]
+    nextPage = () => {
+        let page = this.state.index + 1;
+        if (page === Object.keys(this.state.questions).length) {
+            alert("Done");
+        } else {
+            this.setState({index: page});
+        }
+    };
+
+    prevPage = () => {
+        let page = this.state.index - 1;
+        this.setState({index: page});
+    };
+
+    getQuestionList = () => {
+        let questions = this.state.questions[this.getQuestionKey()];
+        return questions;
+    };
+
+    renderCompletionBar = () => {
+        let spanList = [];
+        for (let i = 0; i < Object.keys(this.state.questions).length; i++) {
+            if (this.state.index >= i) {
+                spanList.push(styles.active);
+            } else {
+                spanList.push(null);
+            }
+        }
+        return (
+            <div className={styles.sectionBar}>
+                {spanList.map((status) => {
+                    return (
+                        <div className={status}></div>
+                    )
+                })}
+            </div>
+        );
     };
 
     render() {
         return (
             <div>
-                <CourseForm title={this.state.questions} questionList={this.getQuestions()}/>
-                <button className={styles.button} onClick={() => this.updateQuestions(1)}>{this.state.index < 2 ? "Next" : "Done"}</button>
+                {this.renderCompletionBar()}
+                <CourseForm title={this.getQuestionKey()} questionList={this.getQuestionList()}/>
+                <button className={styles.button}
+                        onClick={this.nextPage}>{this.state.index < 2 ? "Next" : "Done"}</button>
                 {
-                    this.state.index > 0 ? <button className={styles.button} onClick={() => this.updateQuestions(-1)}>Prev</button> : <div></div>};
-                }
+                    this.state.index > 0 ?
+                        <button className={styles.button} onClick={this.prevPage}>Prev</button> : null};
             </div>
-        );
-    };
+        )
+    }
 }
 
 export default CourseFormPage;
