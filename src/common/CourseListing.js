02b572fd2db777335668f2dd2da4285cf4002c98
Unix-Code <dmalakh@comcast.net>
2019-05-04 16:12:10 -0400

    Added prettierrc + reformatted with new options
    Signed-off-by: Unix-Code <dmalakh@comcast.net>

--- src/common/CourseListing.js
+++ src/common/CourseListing.js
@@ -2,31 +2,31 @@ import React from 'react';
 import styles from './CourseListing.css';
 
 let CourseListing = (title, courses) => {
-    if (!Array.isArray(courses) || !courses.length) {
-        courses = [];
-    }
-    return (
-        <div className={styles.container}>
-            <p className={styles.title}>{title}</p>
-            <div className={styles.courseList}>
-                <hr/>
-                {
-                    courses.map((course) => {
-                        return (
-                            <div className={styles.course}>
-                                <p>
-                                    <span className={styles.num}>{course.num}</span>
-                                    {course.label} - {course.instructor}
-                                    <span className={styles.term}>{course.term}</span>
-                                </p>
-                            </div>
-                        )
-                    })
-                }
-                <hr/>
-            </div>
-        </div>
-    )
+	if (!Array.isArray(courses) || !courses.length) {
+		courses = [];
+	}
+	return (
+		<div className={styles.container}>
+			<p className={styles.title}>{title}</p>
+			<div className={styles.courseList}>
+				<hr />
+				{courses.map(course => {
+					return (
+						<div className={styles.course}>
+							<p>
+								<span className={styles.num}>{course.num}</span>
+								{course.label} - {course.instructor}
+								<span className={styles.term}>
+									{course.term}
+								</span>
+							</p>
+						</div>
+					);
+				})}
+				<hr />
+			</div>
+		</div>
+	);
 };
 
 export default CourseListing;
