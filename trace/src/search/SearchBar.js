1d6e845a071232ce6d270db83f8ade16e869f429
Unix-Code <dmalakh@comcast.net>
2019-05-04 15:36:53 -0400

    Uncommented icon
    Signed-off-by: Unix-Code <dmalakh@comcast.net>

--- trace/src/search/SearchBar.js
+++ trace/src/search/SearchBar.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import styles from "./SearchBar.css";
 
-const SearchBar = props => {
+const SearchBar = () => {
   const [query, setQuery] = useState("");
 
   return (
     <div className={styles.searchBar}>
       <form>
-        {/*<img src={require('./search_icon.svg')}/>*/}
+        <img src={require("./search_icon.svg")} />
         <input onChange={event => setQuery(event.target.value)} />
         <hr />
       </form>
