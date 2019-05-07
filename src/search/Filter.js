a1a6cec72153667300f335ecb7132cff1b6d7587
Unix-Code <dmalakh@comcast.net>
2019-05-07 00:19:34 -0400

    Set up Dropdowns + search page
    Signed-off-by: Unix-Code <dmalakh@comcast.net>

--- src/search/Filter.js
+++ src/search/Filter.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Filter.css';
+import AsyncSelect from 'react-select/lib/Async';
+import axios from 'axios';
 
 
 const Dropdown = (name, options) => {
@@ -16,17 +18,78 @@ const Dropdown = (name, options) => {
 };
 
 const Filter = () => {
-    let options = ["one", "two", "three"];
-    return (
-        <div className={styles.filter}>
-            <p>FILTER</p>
-            <div className={styles.dropdownLists}>
-                {Dropdown("Term", options)}
-                {Dropdown("Department", options)}
-                {Dropdown("Instructor", options)}
-            </div>
-        </div>
-    )
+	const [terms, setTerms] = useState([]);
+	const [departments, setDepartments] = useState([]);
+	const [instructors, setInstructors] = useState([]);
+
+	const departmentToString = d => `${d.title} (${d.code})`;
+	const termToString = t => t.title;
+	const instructorToString = i => `${i.first_name} ${i.last_name}`;
+
+	const getResults = (
+		inputValue = true,
+		resource,
+		resourceToString,
+		withSearch = true
+	) => {
+		if (!inputValue && withSearch) return [];
+		return axios
+			.get(
+				`http://127.0.0.1:5000/api/v1/${resource}` +
+					((withSearch && `?query=${inputValue}`) || '')
+			)
+			.then(res =>
+				((res.data && res.data.data) || []).map(x => ({
+					label: resourceToString(x),
+					value: x
+				}))
+			)
+			.catch(ex => console.error(ex));
+	};
+
+	console.log(terms);
+	console.log(instructors);
+	console.log(departments);
+
+	return (
+		<div className={styles.filter}>
+			<p>FILTER</p>
+			<div className={styles.dropdownLists}>
+				<AsyncSelect
+					isMulti
+					isClearable
+					isSearchable={false}
+					placeholder={'Term'}
+					defaultOptions
+					cacheOptions={true}
+					loadOptions={val =>
+						getResults(val, 'term', termToString, false)
+					}
+					onChange={e => setTerms(e)}
+				/>
+				<AsyncSelect
+					isMulti
+					isSearchable
+					isClearable
+					placeholder={'Department'}
+					loadOptions={val =>
+						getResults(val, 'department', departmentToString)
+					}
+					onChange={e => setDepartments(e)}
+				/>
+				<AsyncSelect
+					isMulti
+					isSearchable
+					isClearable
+					placeholder={'Instructor'}
+					loadOptions={val =>
+						getResults(val, 'instructor', instructorToString)
+					}
+					onChange={e => setInstructors(e)}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Filter;
--- src/search/SearchBar.js
+++ src/search/SearchBar.js
@@ -1,9 +1,5 @@
 import React, { useState } from 'react';
 import styles from './SearchBar.css';
-import SVG from 'react-inlinesvg';
-import KeyImage from './search_icon.svg';
-import axios from 'axios';
-import lodashThrottle from 'lodash/throttle';
 
 const template = {
 	link: 'Report url',
@@ -12,54 +8,14 @@ const template = {
 	instructor: 'B. Lerner'
 };
 
-const SearchBar = ({ setResults }) => {
-	const loadOptions = event => onChange(event);
-	lodashThrottle(loadOptions, 500);
-
-	const parseOptions = options => {
-		return options.data
-			.map(option => {
-				const instructorName =
-					option['instructor']['first_name'].substring(0, 1) +
-					'. ' +
-					option['instructor']['last_name'];
-				const course =
-					option['department']['code'] + ' ' + option['course_id'];
-				return {
-					label: option['name'].toUpperCase(),
-					term: option['term']['title'].toUpperCase(),
-					num: course,
-					instructor: instructorName.toUpperCase()
-				};
-			})
-			.slice(0, 5);
-	};
-
-	const getResults = inputValue => {
-		return axios
-			.get(`http://127.0.0.1:5000/api/v1/course?query=${inputValue}`)
-			.then(res => {
-				return parseOptions(res.data);
-			});
-	};
-	const onChange = e => {
-		const value = e.target.value;
-		const res = getResults(value);
-		console.log(res);
-		res.then(value => {
-			setResults(value);
-		});
-	};
-
-	return (
-		<div className={styles.searchBar}>
-			<form>
-				<img src={require('./search_icon.svg')} />
-				<input onChange={loadOptions} />
-				<hr />
-			</form>
-		</div>
-	);
-};
+const SearchBar = ({ onChange }) => (
+	<div className={styles.searchBar}>
+		<form>
+			<img src={require('./search_icon.svg')} />
+			<input onChange={onChange} />
+			<hr />
+		</form>
+	</div>
+);
 
 export default SearchBar;
