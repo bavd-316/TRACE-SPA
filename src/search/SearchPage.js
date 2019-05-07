bf2a6c39ef9a091ba4232c09f7d63b12157a72a8
Unix-Code <dmalakh@comcast.net>
2019-05-07 00:19:34 -0400

    Set up Dropdowns + search page
    Signed-off-by: Unix-Code <dmalakh@comcast.net>

--- src/search/SearchPage.js
+++ src/search/SearchPage.js
@@ -1,15 +1,48 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+
 import styles from './SearchPage.css';
 import SearchBar from './SearchBar.js';
 import CourseListing from '../common/CourseListing.js';
+import lodashThrottle from 'lodash/throttle';
+import Filter from './Filter';
 
 const SearchPage = () => {
 	const [results, setResults] = useState([]);
 
+	const parseOptions = options => {
+		return ((options && options.data) || [])
+			.map(option => {
+				const instructorName =
+					option['instructor']['first_name'].substring(0, 1) +
+					'. ' +
+					option['instructor']['last_name'];
+				const course =
+					option['department']['code'] + ' ' + option['course_id'];
+				return {
+					label: option['name'].toUpperCase(),
+					term: option['term']['title'].toUpperCase(),
+					num: course,
+					instructor: instructorName.toUpperCase()
+				};
+			})
+			.slice(0, 5);
+	};
+	const search = e => {
+		axios
+			.get(`http://127.0.0.1:5000/api/v1/course?query=${e.target.value}`)
+			.then(res => setResults(parseOptions(res.data)))
+			.catch(ex => console.error(ex));
+		console.log(results);
+	};
+
+	lodashThrottle(search, 500);
+
 	return (
 		<div className={styles.page}>
 			<div className={styles.searchBar}>
-				<SearchBar setResults={setResults} />
+				<SearchBar onChange={search} />
+				<Filter />
 				<div className={styles.searchResults}>
 					<CourseListing title={'SEARCH RESULTS'} courses={results} />
 				</div>
